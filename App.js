49dcd20f9bee9d01bfe517a53f6ca670fe86380f
Dương Quốc Hải <duongquochai123456@gmail.com>
2021-07-15 12:30:06 +0700

    init prj

---
+++ App.js
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './src/screen/home.screen';
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screen/home.screen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text> home.screen </Text>
+    </View>
+  );
+}
